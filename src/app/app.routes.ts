5d432bf5b4a851bc3b036dc4a81f513d8a82b34a
Stryzhak Roman <milon777.official@gmail.com>
2026-04-19 15:52:11 +0300

    Fixed hmr and ci flow

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+// import { App } from './app';
+import { AuthPage } from './auth-page/auth-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthPage,
+    },
+];
